d0fa63dff6ef4d9c40c70f69c4399f362898ec9f
Paul-Sanga <paulsanganyamawi@gmail.com>
2024-02-27 13:10:07 +0300

    modifying the print list method

--- src/custom_linked_list.rs
+++ src/custom_linked_list.rs
@@ -8,13 +8,17 @@ struct Node<T> {
     next: Option<Rc<RefCell<Node<T>>>>,
 }
 
-impl<T> Node<T> {
+impl<T: ToString> Node<T> {
     fn new(value: T) -> Self {
         Node { value, next: None }
     }
+
+    fn get_value_as_string(&self) -> String {
+        self.value.to_string()
+    }
 }
 
-impl <T: std::fmt::Display> std::fmt::Display for Node<T> {
+impl<T: std::fmt::Display> std::fmt::Display for Node<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Node: {}", self.value)
     }
@@ -28,7 +32,7 @@ pub struct CustomLinkedList<T> {
     tail: Option<Rc<RefCell<Node<T>>>>,
 }
 
-impl <T: std::fmt::Display> std::fmt::Display for CustomLinkedList<T> {
+impl<T: std::fmt::Display> std::fmt::Display for CustomLinkedList<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut current_node = self.head.clone();
         while let Some(node) = current_node.clone() {
@@ -98,26 +102,27 @@ impl<T: std::fmt::Display> CustomLinkedList<T> {
         }
     }
 
-    pub fn remove_last(&mut self){
+    pub fn remove_last(&mut self) {
         let mut current_node = self.head.clone();
         let mut prev_node: Option<Rc<RefCell<Node<T>>>> = None;
 
-        if self.count == 0{
+        if self.count == 0 {
             panic!("can not remove from an empty list");
-        }else if self.count == 1 {
+        }
+        if self.count == 1 {
             self.tail = None;
             self.head = None;
             self.count -= 1;
-        }else {
-            while let Some(node) = current_node.clone(){
-                if node.borrow().next.is_some(){
+        } else {
+            while let Some(node) = current_node.clone() {
+                if node.borrow().next.is_some() {
                     prev_node = Some(Rc::clone(&node));
                     current_node = node.borrow().next.clone();
-                }else {
+                } else {
                     break;
                 }
             }
-            if let Some(node) = prev_node.clone(){
+            if let Some(node) = prev_node.clone() {
                 node.borrow_mut().next = None;
                 self.tail = Some(Rc::clone(&node));
                 self.count -= 1;
@@ -126,11 +131,22 @@ impl<T: std::fmt::Display> CustomLinkedList<T> {
     }
 
     pub fn print_list(&self) {
+        let mut output_string = String::from("[");
         let mut current_node = self.head.clone();
-        while let Some(node) = current_node.clone(){
-            println!("{}", node.borrow());
-            current_node = node.borrow().next.clone();
+        while let Some(node) = current_node.clone() {
+            let value = node.borrow().get_value_as_string();
+            if node.borrow().next.is_some() {
+                output_string.push_str(&value);
+                output_string.push(',');
+                output_string.push_str(" ");
+                current_node = node.borrow().next.clone();
+            } else {
+                output_string.push_str(&value);
+                output_string.push(']');
+                break;
+            }
         }
+        println!("{output_string}");
     }
 
     pub fn length(&self) -> usize {
